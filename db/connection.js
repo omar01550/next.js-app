889f40de049e4c025011c0528343eea897d838b7
omar medhat <107444038+omar01550@users.noreply.github.com>
2023-09-30 22:55:56 +0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const url = "mongodb+srv://Omar01550:FDy1cRy0R66uKJhR@cluster0.h9ep7ah.mongodb.net/";
 
-export default async function Connect() {
+export default function Connect() {
     mongoose.connect(url)
         ;
 }
