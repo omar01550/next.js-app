518eb7287b44d390f94099608ed8c84f03d41023
omar medhat <107444038+omar01550@users.noreply.github.com>
2023-09-30 22:54:35 +0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,9 +5,5 @@ const url = "mongodb+srv://Omar01550:FDy1cRy0R66uKJhR@cluster0.h9ep7ah.mongodb.n
 
 export default async function Connect() {
     mongoose.connect(url)
-        .then((res) => {
-            console.log('db connected');
-        }).catch((err) => {
-            console.log("failed to connect with db");
-        });
+        ;
 }
