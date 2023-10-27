b42ab8ff65b8790d79487dd87309ce387ed55965
omar medhat <107444038+omar01550@users.noreply.github.com>
2023-10-27 18:58:22 +0200

    update gallery loading

--- src/app/components/gallary/galarry.jsx
+++ src/app/components/gallary/galarry.jsx
@@ -6,50 +6,61 @@ import ProductImage from '../../../images/suits/pexels-photo-18215399.webp'
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation'
+import Loader from '../loader/loader';
+import ErrorComponent from '../errorComponent/error.jsx'
+
 
 
 const Gallery = () => {
 
     const [images, setImages] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     const router = useRouter();
 
 
     useEffect(() => {
-        fetch("https://next-js-app-1x8d.vercel.app/api/products/get-images")
+
+        fetch("http://localhost:3000/api/products/get-images")
             .then((res) => {
                 return res.json()
             }).then((images) => {
                 setImages(images);
-                console.log(images);
-            }).catch((error) => {
-            
-            console.log(error)
+                setLoading(false);
+            }).catch((err) => {
+                setError(true);
             })
-            
     }, [])
 
 
     return (
-        <div class=" bg-black-500 py-20">
+        <div class=" bg-black-500 py-20 w-[100%]">
 
             <h1 className="gallery-title text-3xl text-center fot-bold capitalize py-10">
                 gallery
             </h1>
 
-            <div className='gallery'>
+            <div className='gallery w-[100%] bg-red-500'>
                 {
-                    images
-                        ? images.map((ele) => {
-                            return (
 
-                                <Image src={ele.image} alt="not found" width={294} height={294} className='cursor-pointer' onClick={() => {
-                                    router.push(`products/${ele._id}`)
-                                }} />
 
-                            )
-                        })
-                        : "no images"
+
+
+
+                    error ? <ErrorComponent />
+                        : loading
+                            ? <Loader />
+                            : images.map((ele) => {
+                                return (
+
+                                    <Image src={ele.image} alt="not found" width={294} height={294} className='cursor-pointer' onClick={() => {
+                                        router.push(`products/${ele._id}`)
+                                    }} />
+
+                                )
+                            })
+
                 }
             </div>
         </div >
