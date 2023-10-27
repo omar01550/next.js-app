7744c918ce2f2166fd552b77bab5d51d8b6c7c16
omar medhat <107444038+omar01550@users.noreply.github.com>
2023-10-27 18:38:59 +0200

    update loading

--- src/app/products/[id]/loading.jsx
+++ src/app/products/[id]/loading.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 const loading = () => {
     return (
-        <div>loading</div>
+        <div className='flex justify-center items-center bg-[#f0f0f0] w-[100%] h-[100vh]'>
+            <CircularProgress />
+        </div>
+
     )
 }
 
