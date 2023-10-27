10e3b04792d734e648a47024a590d5a985c5da6b
omar medhat <107444038+omar01550@users.noreply.github.com>
2023-10-27 19:00:06 +0200

    Create error.jsx

---
+++ src/app/components/errorComponent/error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ErrorComponent = () => {
+    return (
+        <div className='bg-[#f0f0f0] w-[100%] h-[40vh] flex justify-center items-center row-start-1 row-end-3 error-component'>
+
+            <h1 className='text-3xl'>please refresh page</h1>
+
+        </div>
+    )
+}
+
+export default ErrorComponent
+
