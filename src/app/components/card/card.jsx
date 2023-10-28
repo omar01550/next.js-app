269c4507e213f9e1a8ba9ad93d984d019c289c68
omar medhat <107444038+omar01550@users.noreply.github.com>
2023-10-28 09:40:27 +0200

    Update card.jsx

--- src/app/components/card/card.jsx
+++ src/app/components/card/card.jsx
@@ -6,9 +6,12 @@ import { ShoppingBagOutlined } from '@mui/icons-material'
 import { Tooltip } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 
-const Card = ({ title, image, id, describtion, category, price }) => {
+
+
+
+const Card = ({ title, image, id, category, price }) => {
 
     const [SnackBarOPen, setSnackBarOpen] = React.useState(false)
 
@@ -32,19 +35,19 @@ const Card = ({ title, image, id, describtion, category, price }) => {
     return (
 
 
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl m-5">
+        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl m-5">
             <Link href={"/products/" + id}>
-                <img src={image ? image : "https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+                <img src={image ? image : "https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt="Product" className="h-80 w-72 object-cover rounded-t-xl" />
             </Link>
-            <div class="px-4 py-3 w-72">
-                <span class="text-gray-400 mr-3 uppercase text-xs">{category}</span>
-                <p class="text-lg font-bold text-black truncate block capitalize">{title}</p>
-                <div class="flex items-center">
-                    <p class="text-lg font-semibold text-black cursor-auto my-3">${price}</p>
+            <div className="px-4 py-3 w-72">
+                <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                <p className="text-lg font-bold text-black truncate block capitalize">{title}</p>
+                <div className="flex items-center">
+                    <p className="text-lg font-semibold text-black cursor-auto my-3">${price}</p>
                     <del>
-                        <p class="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                        <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
                     </del>
-                    <div class="ml-auto cursor-pointer">
+                    <div className="ml-auto cursor-pointer">
 
                         {/* <Tooltip title="add to cart" arrow>
                             <ShoppingBagOutlined onClick={(params) => {
